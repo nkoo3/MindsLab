3c948a0a98a5f2a10eca9f41256454ff6cbc6995
nkoo3 <58146846+nkoo3@users.noreply.github.com>
2020-08-21 14:05:27 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,39 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import sys
+
+requirement_list = [r.strip() for r in open('requirements.txt', 'r').readlines() if r]
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+else:
+    requirement_list.append('futures')
+
+
+setup(name='ImageScraper',
+      version='2.0.7',
+      install_requires= requirement_list,
+      author='Anantha Natarajan S',
+      author_email='sananthanatarajan12@gmail.com',
+      packages=['image_scraper'],
+      entry_points={
+          'console_scripts': ['image-scraper=image_scraper:main'],
+      },
+      test_suite='tests',
+      url='https://github.com/sananth12/ImageScraper/',
+      description='A simple image scraper to download all images from a given url',
+      classifiers=[
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Topic :: Utilities'
+      ],
+      )
---
+++ test.py
@@ -0,0 +1,71 @@
+import os
+import sys
+import threading
+import image_scraper
+import glob
+from nose.tools import eq_, ok_
+from image_scraper.utils import *
+from image_scraper.exceptions import *
+from image_scraper.progressbar import *
+
+def test_sum():
+    eq_(2+2, 4)
+
+
+def test_get_html_200():
+    eq_(2+2, 4)
+    scraper = ImageScraper()
+    scraper.url = 'http://ananth.co.in/test.html'
+    scraper.get_html()
+    actual_html = u'<html>\n\n<head>\n    \n</head>\n\n<body>\n<img src="images/test4.gif"/>\n<img src="images/test1.jpg"/>\n<img src="images/build.svg"/>\n<img src="images/test.png"/>\n\n</body>\n    \n</html>\n'
+    eq_(scraper.page_html, actual_html)
+
+
+def test_get_html_404():
+    try:
+        scraper = ImageScraper()
+        scraper.url = 'ananth.co.in/test404.html'
+        scraper.get_html()
+        eq_(2, 3, "Page loaded with 200.")  # If the page loads, the test fails.
+    except PageLoadError as e:
+        eq_(e.status_code, 404)
+
+
+def test_process_links():
+    scraper = ImageScraper()
+    scraper.url = 'http://ananth.co.in/test.html'
+    scraper.format_list = ["jpg", "png", "gif", "jpeg"]
+    links = ['a.png', 'b.png', 'c.jpg', 'd.gif', 'f.svg', 'g.svg']
+    r = scraper.process_links(links)
+    correct_res = ['a.png', 'b.png', 'c.jpg', 'd.gif']
+    eq_(r, correct_res)
+
+
+def test_process_links_empty():
+    scraper = ImageScraper()
+    scraper.url = 'http://ananth.co.in/test.html'
+    scraper.format_list = []
+    links = ['a.png', 'b.png', 'c.jpg', 'd.gif', 'f.svg', 'g.svg']
+    r = scraper.process_links(links)
+    correct_res = []
+    eq_(r, correct_res)
+
+
+def test_get_img_list():
+    scraper = ImageScraper()
+    scraper.page_html = u'<html>\n\n<head>\n\n</head>\n\n<body>\n<img src="images/test4.gif"/>\n<img src="images/test1.jpg"/>\n<img src="images/build.svg"/>\n<img src="images/test.png"/>\n\n</body>\n\n</html>\n'
+    scraper.format_list = ["jpg", "png", "gif", "jpeg", "svg"]
+    scraper.url = 'ananth.co.in/test.html'
+    img_list = scraper.get_img_list()
+    actual_list = ['ananth.co.in/images/build.svg', 'ananth.co.in/images/test4.gif', 'ananth.co.in/images/test1.jpg', 'ananth.co.in/images/test.png']
+    eq_(sorted(img_list), sorted(actual_list))
+
+def test_get_img_list_with_pattern():
+    scraper = ImageScraper()
+    scraper.page_html = u'<html>\n\n<head>\n\n</head>\n\n<body>\n<img src="images/test4.gif"/>\n<img src="images/test1.jpg"/>\n<img src="images/build.svg"/>\n<img src="images/test.png"/>\n\n</body>\n\n</html>\n'
+    scraper.format_list = ["jpg", "png", "gif", "jpeg", "svg"]
+    scraper.url = 'ananth.co.in/test.html'
+    scraper.filename_pattern = '[0-9]'
+    img_list = scraper.get_img_list()
+    actual_list = ['ananth.co.in/images/test4.gif', 'ananth.co.in/images/test1.jpg']
+    eq_(sorted(img_list), sorted(actual_list))
